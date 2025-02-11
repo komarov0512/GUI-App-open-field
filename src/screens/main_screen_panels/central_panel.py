b1291b2f02ce61e7fd8d475b5be262b23ecfd6b7
komarov0512 <komarov0512@yandex.ru>
2025-02-12 02:44:44 +0300

    Logic of working with the image added, developed ImageProcessor class

--- src/screens/main_screen_panels/central_panel.py
+++ src/screens/main_screen_panels/central_panel.py
@@ -1,19 +1,37 @@
 import tkinter as tk
+from src.utils import file_load
 
 
 class CentralPanel(tk.Frame):
-    def __init__(self, master, app):
+    def __init__(self, master, app, img_proc, left_panel):
         super().__init__(master)
         self.app = app
+        self.left_panel = left_panel
         self._create_widgets()
+        self.img_proc = img_proc
 
     def _create_widgets(self):
         """Создает элементы центральной панели"""
         self.config(bg="white")
-        # Кнопка "Загрузить файл" по центру
-        self.btn_load = tk.Button(self, text="Загрузить файл",command=self._on_file_load)
+        self.btn_load = tk.Button(self, text="Загрузить файл",command=self._open_image, width=50, height=3)
         self.btn_load.place(relx=0.5, rely=0.5, anchor="center")
 
-    def _on_file_load(self):
-        """Обработчик загрузки файла"""
-        print("Файл загружен!")
+    def _open_image(self):
+        file_path = file_load()
+        self.btn_load.destroy()
+
+        # Панель для отображения изображения
+        self.img_cont = tk.Label(self, bg="white")
+        self.img_cont.pack(fill=tk.BOTH, expand=True)
+
+        self.img_proc.load_image(file_path)
+        self._show_image()
+
+    def _show_image(self):
+        self.img_proc.set_scale_factor(
+            self.winfo_width(),
+            self.winfo_height()
+        )
+        self.img_proc.show_image(self.img_cont)
+        # Активируем кнопки в left_panel
+        self.left_panel.enable_buttons()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
-import tkinter as tk
+import src.config as cfg
+from tkinter import filedialog
 
 
 def center_window(window):
+    """Центрирование окна"""
     window.update_idletasks()
     width = window.winfo_width()
     height = window.winfo_height()
@@ -12,3 +14,13 @@ def center_window(window):
     x = (screen_width // 2) - (width // 2)
     y = (screen_height // 2) - (height // 2)
     window.geometry(f"{width}x{height}+{x}+{y}")
+
+def file_load():
+    """Обработчик загрузки файла"""
+    file_path = filedialog.askopenfilename(
+        title="Выберите видео",
+        filetypes=[("Video files", "*.mp4")]
+    )
+    if file_path:
+        cfg.set_file_path(file_path)
+        return file_path
